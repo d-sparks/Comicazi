43f69a55b907695aaa4168d34f743050ece026c7
Dan Sparks <dan.sparks@clever.com>
2016-08-23 16:09:04 -0700

    Move json logic from endpoints to schemas

--- endpoints.scala
+++ endpoints.scala
@@ -3,9 +3,6 @@ import protocols.http._
 import scala.concurrent._
 import scala.collection._
 import ExecutionContext.Implicits.global
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.scala.DefaultScalaModule
-import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
 import datastore._
 import schemas._
 
@@ -13,14 +10,10 @@ import schemas._
 package endpoints {
 
   class Endpoints(datastore: DataStore) {
-    val mapper = new ObjectMapper() with ScalaObjectMapper
-    mapper.registerModule(DefaultScalaModule)
 
     def postComic(request: HttpRequest) : Future[String] = {
       // TODO: error handling
-      val bodyDataMap = mapper.readValue[mutable.Map[String, Any]](request.body.toString())
-      val comic = new Comic(bodyDataMap)
-      datastore.put(comic, "comics")
+      datastore.put(new Comic(request.body.toString()), "comics")
     }
 
   }
--- schemas.scala
+++ schemas.scala
@@ -1,28 +1,43 @@
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
 import scala.collection._
 
 package schemas {
 
   trait Schema {
     def getData() : Map[String, Any]
+    def getJson() : String
   }
 
-  abstract class SchemaEnforcer(data: mutable.Map[String, Any], schema: Map[String, String]) {
-    schema.foreach({case (k, v) => require(data.contains(k))})
-    data.foreach({case (k, v) =>
+  // note: determine whether this needs to be a case class
+  class Comic(
+    private val json: String
+  ) extends JsonSchemaEnforcer(json, true, Map(
+      "publisher" -> "class java.lang.String",
+      "year" -> "class java.lang.Integer",
+      "mint" -> "class java.lang.Boolean"
+  )) with Schema
+
+  abstract class JsonSchemaEnforcer(
+    private val json: String,
+    private val strict: Boolean,
+    private val schema: Map[String, String]
+  ) {
+    private val mapper = new ObjectMapper() with ScalaObjectMapper
+    mapper.registerModule(DefaultScalaModule)
+    private val d = mapper.readValue[mutable.Map[String, Any]](json)
+    if (strict) { schema.foreach({case (k, v) => require(d.contains(k))}) }
+    d.foreach({case (k, v) =>
       require(schema.get(k) match {
         case Some(expected) => expected == v.getClass().toString()
         case None => false
       })
     })
-    val enforcedData = data.toMap[String, Any]
+    private val enforcedData = d.toMap[String, Any]
+    private val enforcedJson = mapper.writeValueAsString(d)
     def getData() = enforcedData
+    def getJson() = enforcedJson
   }
-
-  // note: determine whether this needs to be a case class
-  class Comic(data: mutable.Map[String, Any]) extends SchemaEnforcer(data, Map(
-    "publisher" -> "class java.lang.String",
-    "year" -> "class java.lang.Integer",
-    "mint" -> "class java.lang.Boolean"
-  )) with Schema
 
 }
