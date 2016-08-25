33d80df317057a4162f71ec87ade6ef8b0e32a09
Dan Sparks <dan.sparks@clever.com>
2016-08-25 13:40:40 -0700

    Refactor schema package

--- src/main/scala/schemas.scala
+++ src/main/scala/schemas.scala
@@ -2,34 +2,50 @@ import json.Converter
 
 package schemas {
 
-  trait Schema {
+  trait Jsonable {
     def toJson() : String
+  }
+
+  case class SchemaValue(value: String, req: Boolean)
+  case class Schema(m: Map[String, SchemaValue])
+
+  object Schemas {
+    val comic = Schema(Map[String, SchemaValue](
+      // Required fields
+      "publisher" -> SchemaValue("class java.lang.String", true),
+      "year" -> SchemaValue("class java.lang.Integer", true),
+      "mint" -> SchemaValue("class java.lang.Boolean", true),
+      // Optional fields
+      "stock" -> SchemaValue("class java.lang.Integer", false)
+    ))
   }
 
   // note: determine whether this needs to be a case class
   class Comic(
     private val json: String
-  ) extends JsonSchemaEnforcer(json, true, Map(
-      "publisher" -> "class java.lang.String",
-      "year" -> "class java.lang.Integer",
-      "mint" -> "class java.lang.Boolean"
-  )) with Schema
+  ) extends JsonSchemaEnforcer(
+    json,
+    Schemas.comic
+  ) with Jsonable
 
   abstract class JsonSchemaEnforcer(
     private val json: String,
-    private val strict: Boolean,
-    private val schema: Map[String, String]
+    private val schema: Schema
   ) {
-    private val d = Converter.toMutableMap(json)
-    if (strict) { schema.foreach({case (k, v) => require(d.contains(k))}) }
-    d.foreach({case (k, v) =>
-      require(schema.get(k) match {
+    private val data = Converter.toMutableMap(json)
+    // throw away fields that are not in the schema
+    data.retain({case (k, v) => schema.m.contains(k)})
+    // check that all required fields are provided
+    for ((k, v) <- schema.m) if(v.req) require(data.contains(k))
+    // enforce type on provided fields
+    for ((k, v) <- data) {
+      require(schema.m.get(k) match {
         case Some(expected) => expected == v.getClass().toString()
         case None => false
       })
-    })
-    private val enforcedData = d.toMap[String, Any]
-    private val enforcedJson = Converter.fromMap(d)
+    }
+    private val enforcedData = data.toMap[String, Any]
+    private val enforcedJson = Converter.fromMap(data)
     def toMap() = enforcedData
     def toJson() = enforcedJson
   }
