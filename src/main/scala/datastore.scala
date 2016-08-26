b7a17a623a0fddba6809be03363379e36e815286
Dan Sparks <dan.sparks@clever.com>
2016-08-26 16:55:10 -0700

    Catch/test invalid json errors in datastore tests

--- src/main/scala/datastore.scala
+++ src/main/scala/datastore.scala
@@ -1,15 +1,14 @@
 import scala.concurrent._
 import ExecutionContext.Implicits.global
-import org.mongodb.scala._
-import org.mongodb.scala.bson._
+import org.mongodb.scala.{MongoClient, Observer, Completed, Document}
 import scala.util.{Success, Failure}
-import org.mongodb.scala.bson.collection.mutable.Document
 
 package datastore {
 
   trait DataStore {
     def put(doc: String, table: String): Future[String]
     def get(query: String, table: String): Future[String]
+    def drop(table: String): Future[Unit]
   }
 
   class MongoStore(mongoUrl: String, mongoDb: String) extends DataStore {
@@ -19,25 +18,39 @@ package datastore {
     def put(doc: String, table: String) : Future[String] = {
       val p = Promise[String]()
       val coll = db.getCollection[Document](table)
-      coll.insertOne(Document(doc)).subscribe(new Observer[Completed] {
-        override def onNext(r: Completed): Unit = println("Comic insert: " + doc)
-        override def onError(e: Throwable): Unit = p.failure(e)
-        override def onComplete(): Unit = p.success(doc)
-      })
-      p.future
+      try {
+        coll.insertOne(Document(doc)).subscribe(new Observer[Completed] {
+          override def onNext(r: Completed): Unit = println(table, "insert", doc)
+          override def onError(e: Throwable): Unit = p.failure(e)
+          override def onComplete(): Unit = p.success(doc)
+        })
+        p.future
+      }
+      catch { case e: Any => { p.failure(e); return p.future } }
     }
 
     def get(query: String, table: String) : Future[String] = {
       val p = promise[String]
-      val coll = db.getCollection[String](table)
+      val coll = db.getCollection[Document](table)
       val bsonQuery = Document(query).toBsonDocument
       val dbResult = coll.find(bsonQuery).toFuture()
       dbResult.onComplete {
-        case Success(results) => { p.success(
-          if(results.length > 0) results(0).toJson() else ""
+        case Success(r) => { p.success(
+          if(r.length > 0) r(0).toJson() else ""
         )}
         case Failure(e) => p.failure(e)
       }
+      p.future
+    }
+
+    def drop(table: String) : Future[Unit] = {
+      val p = Promise[Unit]
+      val coll = db.getCollection[Document](table)
+      coll.drop().subscribe(new Observer[Completed] {
+        override def onNext(r: Completed): Unit = println(table, "dropped")
+        override def onError(e: Throwable): Unit = p.failure(e)
+        override def onComplete(): Unit = p.success()
+      })
       p.future
     }
 
--- src/test/scala/datastore.scala
+++ src/test/scala/datastore.scala
@@ -1,6 +1,9 @@
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration.{Duration}
 import org.scalatest.time.{Span, Seconds, Millis}
 import org.scalatest._
 import org.scalatest.concurrent._
+import scala.util.{Failure, Success}
 import datastore._
 import json.Converter
 
@@ -15,28 +18,48 @@ package datastore {
 
     private val mongo = new MongoStore(
       "mongo://localhost:27017",
-      "comicazi-test"
+      "comicazi-mongostore-test"
     )
-    val doc1 = """{"a":"b"}"""
-    val doc2 = """{"c":"d"}"""
+    val doc = """{"a":"b"}"""
+
+    def blockingCall[T](f: Future[T]) = {
+      Await.ready(f, Duration.Inf).value.get match {
+        case Failure(e) => throw e
+        case Success(result) => Right(result)
+      }
+    }
+
+    def drop(table: String) = blockingCall(mongo.drop(table))
 
     "A put" should "return the json of the doc" in {
-      val dbReturn = mongo.put(doc1, "docs")
+      drop("put-happy")
+      val dbReturn = mongo.put(doc, "put-happy")
       whenReady(dbReturn) { dbOutput =>
-        dbOutput shouldBe doc1
+        dbOutput shouldBe doc
+      }
+    }
+
+    it should "fail for invalid json input" in {
+      drop("put-sad")
+      val dbReturn = mongo.put("{", "put-sad")
+      whenReady(dbReturn.failed) { e =>
+        e shouldBe a [Throwable]
       }
     }
 
     "A get" should "should return the doc in json" in {
-      val dbReturn = mongo.get(doc1, "docs")
+      drop("get-happy")
+      blockingCall(mongo.put(doc, "get-happy"))
+      val dbReturn = mongo.get(doc, "get-happy")
       whenReady(dbReturn) { dbOutput =>
         val filtered = Converter.filterFields(dbOutput, List("_id"))
-        filtered shouldBe doc1
+        filtered shouldBe doc
       }
     }
 
     it should "return empty string if doc not found" in {
-      val dbReturn = mongo.get(doc2, "docs")
+      drop("get-sad")
+      val dbReturn = mongo.get(doc, "get-sad")
       whenReady(dbReturn) { dbOutput =>
         dbOutput shouldBe ""
       }
