233eb199a0419aac6d66c495ebb03d7116bb5025
Dan Sparks <dan.sparks@clever.com>
2016-08-28 11:33:19 -0700

    Add Base64 encoding helpers

--- src/main/scala/json.scala
+++ src/main/scala/json.scala
@@ -1,29 +1,25 @@
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
+import sun.misc.{BASE64Encoder, BASE64Decoder}
 import scala.collection._
 
 package json {
 
   object JSON {
-
     private val mapper = new ObjectMapper() with ScalaObjectMapper
     mapper.registerModule(DefaultScalaModule)
 
     def fromMap(m: Map[String, Any]) = mapper.writeValueAsString(m)
-
     def toMutableMap(json: String) = {
       mapper.readValue[mutable.Map[String, Any]](json)
     }
-
     def toMap(json: String) = toMutableMap(json).toMap[String, Any]
-
     def filter(json: String, fields: List[String]) = {
       val m = toMutableMap(json)
       for (field <- fields) { m.remove(field) }
       fromMap(m.toMap[String,Any])
     }
-
     def extend(json: String, extension: String) = {
       val m = toMutableMap(json)
       val mExt = toMutableMap(extension)
@@ -32,17 +28,21 @@ package json {
       }
       fromMap(m.toMap)
     }
-
     def getKeys(json: String) = {
       JSON.toMutableMap(json).keys.toList.sorted.mkString(",")
     }
-
     def project(json: String, fields: List[String]) = {
       fromMap(toMutableMap(json).retain({(k: String, v: Any) =>
         fields.contains(k)
       }).toMap)
     }
+  }
 
+  object Base64 {
+    private val encoder = new BASE64Encoder()
+    private val decoder = new BASE64Decoder()
+    def encode(s: String) = encoder.encode(s.toCharArray.map(_.toByte))
+    def decode(s: String) = new String(decoder.decodeBuffer(s).map(_.toChar))
   }
 
 }
--- src/test/scala/json.scala
+++ src/test/scala/json.scala
@@ -1,8 +1,8 @@
 import org.scalatest._
-import json.JSON
+import json.{JSON, Base64}
 
 package json {
-  class ProjectSpec extends FlatSpec with Matchers {
+  class JSONSpec extends FlatSpec with Matchers {
     "project" should "project onto the given fields" in {
       val json = """{"a":"b","c":"d"}"""
       val fields = List("a", "e")
@@ -13,6 +13,13 @@ package json {
       val json = """{"a":"b","c":"d"}"""
       val fields = List()
       JSON.project(json, fields) shouldBe """{}"""
+    }
+  }
+
+  class BSONSpec extends FlatSpec with Matchers {
+    "decode(encode(s)" should "be s" in {
+      val s = "s9df8asd89fhjaklsj#(&H@D<KK%THK"
+      Base64.decode(Base64.encode(s)) shouldBe s
     }
   }
 }
