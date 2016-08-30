84bca5420172fa19d34ad372242f03769c244749
Dan Sparks <dan.sparks@clever.com>
2016-08-30 07:35:38 -0700

    Break postComic into flatMap composition of steps

--- src/main/scala/endpoints.scala
+++ src/main/scala/endpoints.scala
@@ -4,44 +4,41 @@ import scala.concurrent._
 import scala.collection.mutable.MutableList
 import ExecutionContext.Implicits.global
 import json.{JSON, Base64}
-import datastore._
+import datastore.DataStore
 import schemas._
 
 package endpoints {
 
-  class Endpoints(datastore: DataStore) {
+  class Endpoints(db: DataStore) {
 
-    // todo: break the flatmaps up into individual functions
-    // also: all of this logic should be in the notification worker
     def postComic(request: HttpRequest) : Future[String] = {
       val body = request.body.toString()
-      val comic = new Comic(body).toJson()
-      // * STEP 1 * find all QueryPattern's
-      datastore.get("{}", "querypatterns").flatMap { querypatterns =>
-      // * STEP 2 * create relevant PendingQuery's
-        val pqs = MutableList[PendingQuery]()
-        for (querypattern <- querypatterns) {
-          val qp = new QueryPattern(querypattern)
-          val fieldsString = qp.toMap().get("querypattern") match {
-            case Some(v) => v.asInstanceOf[String]
-            case None => throw new Exception("Fire! Fire! Fire!")
-          }
-          val searchFields = fieldsString.split(",").toList
-          val querystring = JSON.project(comic, searchFields)
-          pqs += new PendingQuery(querystring, comic)
+      val comicJson = new Comic(body).toJson()
+      db.get("{}", "querypatterns") flatMap insertQueryPatterns(comicJson)
+    }
+
+    def insertQueryPatterns(comicJson: String)(qpsJson: List[String]) = {
+      // Turn QueryPatterns (qp) into PendingQueries (pq)
+      val pqsJson = qpsJson.map {case(querypattern: String) =>
+        val qp = new QueryPattern(querypattern)
+        val fieldsString = qp.toMap().get("querypattern") match {
+          case Some(v) => v.asInstanceOf[String]
+          case None => throw new Exception("JsonSchemaEnforcer fail: PendingQuery")
         }
-        val pqsJson = pqs.map({
-          (pq: PendingQuery) => pq.toJson()
-        })
-        datastore.putMany(pqsJson.toList, "pendingqueries")
-      }.flatMap { _ =>
-      // * STEP 3 * create a notification job
-        val nj = new NotificationJob(comic, 1)
-        datastore.put(nj.toJson, "notificationjobs")
-      }.flatMap { _ =>
-      // * STEP 4 * add the comic to inventory
-        datastore.put(comic, "comics")
-      }
+        val searchFields = fieldsString.split(",").toList
+        val querystring = JSON.project(comicJson, searchFields)
+        new PendingQuery(querystring, comicJson).toJson
+      }.toList
+      db.putMany(pqsJson, "pendingqueries") flatMap createJob(comicJson)
+    }
+
+    def createJob(comicJson: String)(putResult: String) = {
+      val nj = new NotificationJob(comicJson, 1)
+      db.put(nj.toJson, "notificationjobs") flatMap insertComic(comicJson)
+    }
+
+    def insertComic(comicJson: String)(putResult: String) = {
+      db.put(comicJson, "comics")
     }
 
     def postSubscription(request: HttpRequest) : Future[String] = {
@@ -49,8 +46,8 @@ package endpoints {
       val sub = new Subscription(body)
       val qpJson = s"""{"querypattern":"${sub.querypattern}"}"""
       val querypattern = new QueryPattern(qpJson)
-      datastore.put(querypattern.toJson(), "querypatterns").flatMap { _ =>
-        datastore.put(sub.toJson(), "subscriptions")
+      db.put(querypattern.toJson(), "querypatterns").flatMap { _ =>
+        db.put(sub.toJson(), "subscriptions")
       }
     }
 
